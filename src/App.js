cae26df0642a851727374ebf15247e7da30b2dae
seals <carolgaozx@gmail.com>
2023-03-13 16:20:26 -0400

    added button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isToggleOn: true
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-  //handler goes here
+    //handler goes here
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
 
-  render() {
-    return (
-      <div>
-        <p>Greetings!</p>
-        <p>Button goes here</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <p>Greetings!</p>
+                <p>
+                    <button onClick={this.handleClick}>
+                        {this.state.isToggleOn ? 'click me' : 'thanks'}
+                    </button>
+                </p>
+            </div>
+        );
+    }
 }
